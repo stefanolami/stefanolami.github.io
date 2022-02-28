9273f0fde99bd29b5b417a1808c74867a0144005
stefanolami <stefanolami90@gmail.com>
2022-02-28 21:36:36 +0100

    mobile layout started

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 const figCaption = document.querySelector('.profile-pic-caption')
 
-figCaption.addEventListener('mouseenter', () => {
-    figCaption.textContent = 'Click on my face to know more about me!';
-});
-
-figCaption.addEventListener('mouseleave', () => {
-    figCaption.textContent = 'Click on my face to know...';
-});
 
 
+
+
+
+const mqLarge  = window.matchMedia( '(min-width: 700px)' );
+
+ if (mqLarge.matches) {
+     console.log('matches!')
+        figCaption.addEventListener('mouseenter', () => {
+            figCaption.textContent = 'Click on my face to know more about me!';
+        });
+        
+        figCaption.addEventListener('mouseleave', () => {
+            figCaption.textContent = 'Click on my face to know...';
+        });
+    }
